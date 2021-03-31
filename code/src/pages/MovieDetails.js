8554150e017c40f8e0dbe06ac32efb4d12ec9832
Irina <jekabsone.irina@gmail.com>
2021-03-31 16:57:14 +0200

    MovieDetails page fetching movie details and returning movie details

---
+++ code/src/pages/MovieDetails.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+export const MovieDetails = () => {
+  const [details, setDetails] = useState([])
+  const {movie_id} = useParams()
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=2fbbfc5b76142a312e36c1069a8f5912&language=en-US`)
+      .then((res) => res.json())
+      .then((data) => {
+        setDetails(data)
+        console.log(data)
+      })
+  }, [movie_id])
+
+return(
+  <>
+    <div className='details-container'>
+      <h2 className='movie-title'>{details.original_title}</h2>
+      <h2 className='overview'>{details.overview}</h2>
+      <h2 className='rating'>{details.vote_average}</h2>
+      <img className='small-movie-poster' src={`https://image.tmdb.org/t/p/w300${details.poster_path}`} alt=" small movie poster"/>
+    </div>
+    <img className='background-movie-poster' src={`https://image.tmdb.org/t/p/w1280${details.poster_path}`} alt="background movie poster"/>
+  </>
+)
+}
